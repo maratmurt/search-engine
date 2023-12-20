e57d1d9945041d5ecc657c40e0d5a7d0b6060c17
Marat Murtuzaliev <marat.murtuzaliev@gmail.com>
2023-12-21 00:01:17 +0300

    Revert "startIndexing"
    This reverts commit 0639083d3decd813e58c6e6c03afb37fab76c89b.

--- src/main/java/searchengine/controllers/ApiController.java
+++ src/main/java/searchengine/controllers/ApiController.java
@@ -1,79 +1,24 @@
 package searchengine.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import searchengine.config.*;
 import searchengine.dto.statistics.StatisticsResponse;
-import searchengine.model.Page;
-import searchengine.model.SiteEntity;
-import searchengine.model.Status;
-import searchengine.services.*;
-
-import java.util.concurrent.*;
-import java.util.logging.Logger;
+import searchengine.services.StatisticsService;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
     private final StatisticsService statisticsService;
-    private final SitesList sitesList;
-    private final RepositoryService repositoryService;
 
-    @Autowired
-    public ApiController(StatisticsService statisticsService, SitesList sitesList, RepositoryService repositoryService) {
+    public ApiController(StatisticsService statisticsService) {
         this.statisticsService = statisticsService;
-        this.sitesList = sitesList;
-        this.repositoryService = repositoryService;
     }
 
     @GetMapping("/statistics")
     public ResponseEntity<StatisticsResponse> statistics() {
         return ResponseEntity.ok(statisticsService.getStatistics());
-    }
-
-    @GetMapping(value = "/startIndexing")
-    public ResponseEntity<String> startIndexing() {
-        Logger logger = Logger.getLogger("CTR-log");
-
-        int taskCount = sitesList.getSites().size();
-
-        ExecutorService executor = Executors.newFixedThreadPool(taskCount);
-
-        for (Site siteConfig : sitesList.getSites()) {
-            SiteEntity siteRecord = repositoryService.findSiteByUrl(siteConfig.getUrl());
-            if (siteRecord != null) {
-                Iterable<Page> pages = repositoryService.findPagesBySite(siteRecord);
-                if (pages != null) {
-                    repositoryService.deleteAll(pages);
-                }
-                repositoryService.delete(siteRecord);
-            }
-
-            SiteEntity site = new SiteEntity();
-            site.setName(siteConfig.getName());
-            site.setUrl(siteConfig.getUrl());
-            site.setStatus(Status.INDEXING);
-            repositoryService.save(site);
-
-            PageCollector collector = new PageCollector(site, "/", repositoryService);
-
-            CompletableFuture<SiteEntity> future = new CompletableFuture<>();
-            StatusChecker checker = new StatusChecker(future, repositoryService);
-            executor.execute(checker);
-
-            new Thread(()->{
-                ForkJoinPool pool = new ForkJoinPool(8);
-                pool.invoke(collector);
-                future.complete(site);
-            }).start();
-        }
-
-        executor.shutdown();
-
-        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/searchengine/model/Status.java
+++
@@ -1,5 +0,0 @@
-package searchengine.model;
-
-public enum Status {
-    INDEXING, INDEXED, FAILED
-}
