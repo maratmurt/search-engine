96362044f9c6787897ff2199c8fd19a8c8f0a913
Marat Murtuzaliev <marat.murtuzaliev@gmail.com>
2024-01-25 16:48:29 +0300

    fix: blank DB statistics error

--- src/main/java/searchengine/services/StatisticsServiceImpl.java
+++ src/main/java/searchengine/services/StatisticsServiceImpl.java
@@ -1,6 +1,7 @@
 package searchengine.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import searchengine.config.Site;
 import searchengine.config.SitesList;
@@ -18,7 +19,9 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class StatisticsServiceImpl implements StatisticsService {
@@ -49,17 +52,23 @@ public class StatisticsServiceImpl implements StatisticsService {
             DetailedStatisticsItem item = new DetailedStatisticsItem();
             item.setName(siteConfig.getName());
             item.setUrl(siteConfig.getUrl());
-            SiteEntity site = siteRepository.findByUrl(siteConfig.getUrl()).orElseThrow();
-            int pages = pageRepository.countBySite(site);
-            int lemmas = lemmaRepository.countBySite(site);
-            item.setPages(pages);
-            item.setLemmas(lemmas);
-            item.setStatus(site.getStatus().toString());
-            item.setError(site.getLastError());
-            ZonedDateTime zdt = ZonedDateTime.of(site.getStatusTime(), ZoneId.systemDefault());
-            item.setStatusTime(zdt.toEpochSecond());
-            total.setPages(total.getPages() + pages);
-            total.setLemmas(total.getLemmas() + lemmas);
+
+            SiteEntity site;
+            try {
+                site = siteRepository.findByUrl(siteConfig.getUrl()).orElseThrow();
+                int pages = pageRepository.countBySite(site);
+                int lemmas = lemmaRepository.countBySite(site);
+                item.setPages(pages);
+                item.setLemmas(lemmas);
+                item.setStatus(site.getStatus().toString());
+                item.setError(site.getLastError());
+                ZonedDateTime zdt = ZonedDateTime.of(site.getStatusTime(), ZoneId.systemDefault());
+                item.setStatusTime(zdt.toEpochSecond());
+                total.setPages(total.getPages() + pages);
+                total.setLemmas(total.getLemmas() + lemmas);
+            } catch (NoSuchElementException e) {
+                log.error(e.getMessage());
+            }
             detailed.add(item);
         }
 
