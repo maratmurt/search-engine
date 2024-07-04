a7234697c903a3028ee4d4626458b4dc13e0e75d
Marat <marat.murtuzaliev@gmail.com>
2024-07-04 23:17:34 +0300

    fix: lemmas update synchronized

--- src/main/java/searchengine/utils/IndexProcessor.java
+++ src/main/java/searchengine/utils/IndexProcessor.java
@@ -35,25 +35,24 @@ public class IndexProcessor extends Thread {
             Map<String, Double> lemmaRankMap = lemmatizer.buildLemmaRankMap(text);
             List<String> lemmas = lemmaRankMap.keySet().stream().toList();
 
-            // update existing lemmas
-            List<LemmaDto> existingLemmas = lemmaDao.findAllByLemmaAndSiteId(lemmas, siteId);
-            existingLemmas.forEach(lemma -> lemma.setFrequency(lemma.getFrequency() + 1));
-            lemmaDao.updateAll(existingLemmas);
+            synchronized (lemmaDao) {
+                List<LemmaDto> existingLemmas = lemmaDao.findAllByLemmaAndSiteId(lemmas, siteId);
+                existingLemmas.forEach(lemma -> lemma.setFrequency(lemma.getFrequency() + 1));
+                lemmaDao.updateAll(existingLemmas);
 
-            // create new lemmas
-            List<String> existingLemmaWords = existingLemmas.stream().map(LemmaDto::getLemma).toList();
-            List<LemmaDto> newLemmas = lemmas.stream()
-                    .filter(lemma -> !existingLemmaWords.contains(lemma))
-                    .map(lemma -> {
-                LemmaDto lemmaDto = new LemmaDto();
-                lemmaDto.setLemma(lemma);
-                lemmaDto.setSiteId(siteId);
-                lemmaDto.setFrequency(1);
-                return lemmaDto;
-            }).toList();
-            lemmaDao.saveAll(newLemmas);
+                List<String> existingLemmaWords = existingLemmas.stream().map(LemmaDto::getLemma).toList();
+                List<LemmaDto> newLemmas = lemmas.stream()
+                        .filter(lemma -> !existingLemmaWords.contains(lemma))
+                        .map(lemma -> {
+                            LemmaDto lemmaDto = new LemmaDto();
+                            lemmaDto.setLemma(lemma);
+                            lemmaDto.setSiteId(siteId);
+                            lemmaDto.setFrequency(1);
+                            return lemmaDto;
+                        }).toList();
+                lemmaDao.saveAll(newLemmas);
+            }
 
-            // create indexes
             List<IndexDto> indexes = lemmaDao.findAllByLemmaAndSiteId(lemmas, siteId).stream().map(lemma -> {
                 IndexDto index = new IndexDto();
                 index.setPageId(page.getId());
